474aa44b653e58b3fd561987a2861475801c9efa
Pavlos Karalis <52301933+pavloskaralis@users.noreply.github.com>
2019-09-21 18:05:04 -0400

    Update app.js

--- index/app.js
+++ index/app.js
@@ -1,4 +1,4 @@
-/////Items/////
+/////Items Objects/////
 const teeth = {
     text: "Teeth",
     value: 0,
@@ -31,11 +31,11 @@ const kids = {
     image: "img/kids.jpg",
 }
 
-///// Shop to Inventory Variables (buyItem Function)/////
+///// shopToInventory()'s buyItem() Variables/////
 const objectArray = [teeth, razor, goat, mower, kids];
 let nextItemIndex = 0; 
 
-/////Score Counter/////
+/////Score Counter Variables/////
 let lawnsCut = 0;
 let cash = 0; 
 let itemsOwned = 0;
@@ -51,7 +51,7 @@ const background = document.getElementById("background");
 
 /////Stored Prompts/////
 const winConditionsPrompt = () => {
-    promptArea.innerHTML = `<span class="altH1">Win Conditions:</span><br>Buy all items and earn $1000.`
+    promptArea.innerHTML = `<span class="altH2">Win Conditions:</span><br>Buy all items and earn $1000.`
 }
 const howToEquipPrompt = () => {
     promptArea.innerHTML = `Click an inventory item to equip it,<br>Then click the grass to begin landscaping.`
@@ -72,7 +72,7 @@ const winningMessageTwo = () => {
     promptArea.innerHTML = `You are a master <span class="title">LANDSCAPER</span>`;
 }
 const winningMessageThree = () => {
-    promptArea.innerHTML = `Collect your prize by clicking <a href="https://images-na.ssl-images-amazon.com/images/I/71Ou%2BZt6SPL._SL1500_.jpg" class="altH1">here!</a>`;
+promptArea.innerHTML = `Collect your prize by clicking <a href="https://www.amazon.com/Pack-Scope-Mouthwash-Outlast-Lasting/dp/B017HTAOSY/ref=sr_1_6?crid=1HMVO25MM0FM5&keywords=scope+mouthwash&qid=1569102917&s=gateway&sprefix=scope+mout%2Caps%2C127&sr=8-6" class="altH1">here!</a>`;
 }
 const blankPrompt = () => {
     promptArea.innerHTML = ``
@@ -90,17 +90,18 @@ const start = () => {
     itemToInventory(teeth);
     nextItemIndex += 1; 
     grass.addEventListener("click", mowGrass);
-    let noClickAtStart =() => {
-        for (let i = 0; i < 5; i++){
-            document.getElementsByClassName("circle")[i].classList.toggle("noclick");
-        }
-        grass.classList.toggle("noclick");
-    }
     noClickAtStart();
     setTimeout(noClickAtStart, 6000);
     setTimeout(winConditionsPrompt, 3000);
     setTimeout(howToEquipPrompt, 6000);
     
+}
+
+const noClickAtStart =() => {
+    for (let i = 0; i < 5; i++){
+        document.getElementsByClassName("circle")[i].classList.toggle("noclick");
+    }
+    grass.classList.toggle("noclick");
 }
 
 const itemToInventory = (object) => {  
@@ -119,7 +120,7 @@ const itemToInventory = (object) => {
     inventoryItem.addEventListener("click",equipItem);
 
     let inventoryItemText = document.createElement("h2");
-    inventoryItemText.innerHTML = `${object.text} <span class="altH2">Earn: $${object.earningPower}</span>`;
+    inventoryItemText.innerHTML = `${object.text} <span class="altH2"> +$${object.earningPower}</span>`;
     inventorySlots.appendChild(inventoryItemText); 
     
     let divTwo = document.createElement("div");				
@@ -152,16 +153,16 @@ const itemToShop = (object, shopIndex) => {
     shopItem.addEventListener("click",buyItem);
 
     let inventoryItemText = document.getElementsByTagName("h2")[shopIndex];
-    inventoryItemText.innerHTML = `${object.text} <span class="altH2">Buy $${object.value}</span>`;
+    inventoryItemText.innerHTML = `${object.text} <span class="altH2">-$${object.value}</span>`;
 }
 
 const mowGrass = () => {
-    const noClick = () => {
+    const noClickGrass = () => {
         grass.classList.remove("noclick");
     }
-    const unMow = (object) => {
+    const unMowGrass = (object) => {
             grass.classList.toggle("unMow");
-            setTimeout(noClick, 900);   
+            setTimeout(noClickGrass, 700);   
             grass.classList.remove("mow"); 
         }
     if (document.getElementsByClassName("equip")[0]){
@@ -170,14 +171,14 @@ const mowGrass = () => {
         grass.classList.toggle("noclick");
         grass.classList.toggle("mow");
         setTimeout(updateScore, 500);
-        setTimeout(unMow, 1000);  
+        setTimeout(unMowGrass, 1000);  
         setTimeout(checkForWin, 1000);  
     } else {
         mustEquipPrompt(); 
     }
 }
 
-let updateScore = () => {     
+const updateScore = () => {     
     let currentEarningPower = document.getElementsByClassName("equip")[0].getAttribute("data-earningPower");
     currentEarningPower = parseInt(currentEarningPower,10);
     lawnsCutText.innerHTML = `Lawns Cut: ${lawnsCut += 1}`;
@@ -185,10 +186,9 @@ let updateScore = () => {
     promptArea.innerHTML = `You earned $${currentEarningPower}!`;
 }       
 
-let checkForWin = () => {
+const checkForWin = () => {
     if (cash >= 1000 && itemsOwned === 5) {
         background.style.backgroundImage = `url("img/win.jpg")`;
-        grass.classList.toggle("noclick");
         background.removeChild(grass);
         winningMessageOne();
         setTimeout(winningMessageTwo, 3000);
